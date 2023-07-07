7dd7fb3531f761fa25f7587147cfa13443bc1e69
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-07-07 19:08:40 +0300

    Added Request Profiling using middleware

---
+++ ExpenseTracking.WebAPI/Middlewares/RequestTrackingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace ExpenseTracking.WebAPI.Middlewares
+{
+    public class RequestTrackingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestTrackingMiddleware> _logger;
+
+        public RequestTrackingMiddleware(RequestDelegate next, ILogger<RequestTrackingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            try
+            {
+                await _next(context);
+            }
+            finally
+            {
+                stopwatch.Stop();
+
+                var request = context.Request;
+                var response = context.Response;
+
+                _logger.LogInformation(
+                    "Request: {requestMethod} {requestPath} | " +
+                    "Response: {responseStatusCode} | " +
+                    "Duration: {responseDuration} ms"
+                    , request.Method, request.Path, response.StatusCode, stopwatch.Elapsed.TotalMilliseconds);
+            }
+        }
+
+    }
+
+    public static class RequestTrackingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestTrackingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestTrackingMiddleware>();
+        }
+    }
+}
--- ExpenseTracking.WebAPI/Program.cs
+++ ExpenseTracking.WebAPI/Program.cs
@@ -105,6 +105,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseRequestTrackingMiddleware();
 app.UseHangfireDashboard();
 app.UseHttpsRedirection();
 
